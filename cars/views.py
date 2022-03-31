d296778d00798abb16911f19d0dbcbf2a2241fe1
Andre <andrebennett106@gmail.com>
2022-03-31 12:20:22 -0400

    Created New View Function for Getting Care by ID + Added New URLS Patj fpr car_detail View Function

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,10 @@
+from multiprocessing import reduction
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import CarSerializer
 from .models import Car
+from cars import serializers
 
 @api_view(['GET', 'POST'])
 def cars_list(request):
@@ -15,9 +18,16 @@ def cars_list(request):
 
     elif request.method == 'POST':
         serializer = CarSerializer(data=request.data)
-        if serializer.is_valid() == True:
-            serializer.save()
-            return Response(serializer.data, status=201)
-        else:
-            return Response(serializer.errors, status=400)
-
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+       
+@api_view(['GET'])
+def car_detail(request, pk):
+    try:
+        car = Car.objects.get(pk=pk)
+        serializer = CarSerializer(car)
+        return Response(serializer.data)
+        
+    except Car.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND);
